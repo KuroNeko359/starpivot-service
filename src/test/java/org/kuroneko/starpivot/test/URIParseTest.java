6f6b39ffaaeb86f047ca4814bcc874bef0d76ef7
KuroNeko359 <jz2077056966@gmail.com>
2025-03-16 12:48:56 +0800

    Update URIParseTest.java

--- src/test/java/org/kuroneko/starpivot/test/URIParseTest.java
+++ src/test/java/org/kuroneko/starpivot/test/URIParseTest.java
@@ -1,23 +1,22 @@
 package org.kuroneko.starpivot.test;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.fs.Path;
 import org.junit.jupiter.api.Test;
 import org.kuroneko.starpivot.entity.hadoop.File;
+import org.kuroneko.starpivot.entity.hadoop.HadoopFile;
 import org.kuroneko.starpivot.services.HdfsClientService;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
+import java.net.*;
 import java.util.List;
 
 public class URIParseTest {
     HdfsClientService hdfsClientService = new HdfsClientService();
 
     public URIParseTest() throws IOException, URISyntaxException, InterruptedException {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
     }
 
     @Test
@@ -51,5 +50,19 @@ public class URIParseTest {
     public void testGetFiles() throws URISyntaxException, IOException, InterruptedException {
         List<File> fileSystemItemCurrentPath = hdfsClientService.getFileSystemItemCurrentPath("/");
         fileSystemItemCurrentPath.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetFile() throws URISyntaxException, IOException, InterruptedException {
+
+        String path = new URL("hdfs://hadoop102:9000/新建 Microsoft PowerPoint 演示文稿.pptx").getPath();
+        System.out.println(path);
+    }
+
+    @Test
+    public void testPathToUri() {
+        Path path = new Path("hdfs://hadoop102:9000/files/新建 Microsoft PowerPoint 演示文稿.pptx");
+        URI uri = path.toUri();
+        System.out.println(uri.getPath());
     }
 }
